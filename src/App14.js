63974fd379ec40a95b050c2c68fe917be1e1ea8b
pavansailyros <pavansai@lyrostech.com>
2024-03-09 15:57:31 +0530

    albums

--- src/App14.js
+++ src/App14.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Feeds from "./Feeds";
 import Post from "./Post";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Albums from "./Albums";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 export default function App14() {
   return (
     <div>
       <Router>
-        <Link to="/">Feeds</Link> | <Link to="/post">Post</Link> <Link to ="/albums"></Link>
+        <Link to="/">Feeds</Link> | <Link to="/post">Post</Link> | <Link to="/albums">Albums</Link>
 
         <Routes>
-            <Route path="/" index element={<Feeds/>}></Route>
-            <Route path="/post" element={<Post/>}></Route>
-            <Route path="/albums" element={<Albums/>}></Route>
+          <Route path="/" index element={<Feeds />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/albums" element={<Albums />} />
         </Routes>
       </Router>
     </div>
